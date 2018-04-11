2fcd05b36ebae861c74c953c5e7241752ad9a525
TaslimOseni <tasleemoseni@gmail.com>
2018-04-11 10:26:40 +0100

    Initial commit

---
+++ app/src/main/java/apps/amazon/com/dide/OpenActivity.java
@@ -0,0 +1,13 @@
+package apps.amazon.com.dide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open);
+    }
+}
