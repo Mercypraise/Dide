27ed5f456040646cc712b095611c4ecd722b4b8c
TaslimOseni <tasleemoseni@gmail.com>
2018-04-11 15:05:47 +0100

    Splash and login page ready.

--- app/src/main/java/apps/amazon/com/dide/OpenActivity.java
+++ app/src/main/java/apps/amazon/com/dide/OpenActivity.java
@@ -1,13 +1,30 @@
 package apps.amazon.com.dide;
 
+
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class OpenActivity extends AppCompatActivity {
+public class OpenActivity extends AppCompatActivity{
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open);
+
+        new CountDownTimer(3000, 1000){
+            @Override
+            public void onTick(long millisUntilFinished){
+
+            }
+
+
+            @Override
+            public void onFinish(){
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        }.start();
     }
 }
