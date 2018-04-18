a2ffa07ae68bbcaf7417384ecea2ad0b9553480d
TaslimOseni <tasleemoseni@gmail.com>
2018-04-18 01:56:55 +0100

    UI fixes complete!

--- app/src/main/java/apps/amazon/com/dide/activities/SplashActivity.java
+++ app/src/main/java/apps/amazon/com/dide/activities/SplashActivity.java
@@ -1,23 +1,12 @@
 package apps.amazon.com.dide.activities;
 
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.ObjectAnimator;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Animatable;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-
 import apps.amazon.com.dide.R;
+
 
 public class SplashActivity extends AppCompatActivity{
 
@@ -28,31 +17,32 @@ public class SplashActivity extends AppCompatActivity{
         setContentView(R.layout.activity_splash);
 
 
-        new CountDownTimer(1500, 1000){
+        new CountDownTimer(1700, 1000){
             @Override
             public void onTick(long millisUntilFinished){
 
             }
 
             @Override
-            public void onFinish() {
-                ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(findViewById(R.id.image), View.ALPHA, 1.0f, 0.7f);
-                objectAnimator.setDuration(1000).addListener(new AnimatorListenerAdapter(){
-
-                    @Override
-                    public void onAnimationEnd(Animator animation){
-                        Log.d("CHECK", "got here");
-                        startActivity(new Intent(getApplicationContext(), TwinActivity.class));
-                    }
-
-                    @Override
-                    public void onAnimationResume(Animator animation) {
-                        onFinish();
-                    }
-
-                });
-
-                objectAnimator.start();
+            public void onFinish(){
+//                ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(findViewById(R.id.image), View.ALPHA, 1.0f, 0.7f);
+//                objectAnimator.setDuration(1000).addListener(new AnimatorListenerAdapter(){
+//
+//                    @Override
+//                    public void onAnimationEnd(Animator animation){
+//                        Log.d("CHECK", "got here");
+//                        startActivity(new Intent(getApplicationContext(), TwinActivity.class));
+//                    }
+//
+//                    @Override
+//                    public void onAnimationResume(Animator animation) {
+//                        onFinish();
+//                    }
+//
+//                });
+//
+//                objectAnimator.start();
+                startActivity(new Intent(getApplicationContext(), TwinActivity.class));
             }
         }.start();
     }
--- app/src/main/java/apps/amazon/com/dide/fragments/MotivationFragment.java
+++ app/src/main/java/apps/amazon/com/dide/fragments/MotivationFragment.java
@@ -3,7 +3,9 @@ package apps.amazon.com.dide.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +20,7 @@ import apps.amazon.com.dide.R;
 
 public class MotivationFragment extends Fragment{
 
+    int i = 0;
 
     public MotivationFragment(){
 
@@ -43,17 +46,32 @@ public class MotivationFragment extends Fragment{
 
 
         int[] quotes = {R.drawable.b1, R.drawable.b2, R.drawable.b3, R.drawable.b4,
-                        R.drawable.b6, R.drawable.b7, R.drawable.b10,
-                        R.drawable.b12, R.drawable.b16, R.drawable.b18, R.drawable.b19,
-                        R.drawable.b21, R.drawable.b22, R.drawable.b23, R.drawable.b24, R.drawable.b25,
-                        R.drawable.b27, R.drawable.b28, R.drawable.b30,
-                        R.drawable.b31, R.drawable.b32, R.drawable.b34, R.drawable.b35,
-                        R.drawable.b36};
+                        R.drawable.b6, R.drawable.b7, R.drawable.b10, R.drawable.b12,
+                        R.drawable.b16, R.drawable.b18, R.drawable.b19, R.drawable.b21,
+                        R.drawable.b22, R.drawable.b23, R.drawable.b24, R.drawable.b25,
+                        R.drawable.b27, R.drawable.b28, R.drawable.b30, R.drawable.b31,
+                        R.drawable.b32, R.drawable.b34, R.drawable.b35, R.drawable.b36};
 
-        Random random = new Random();
-        int rand = random.nextInt(quotes.length);
+        final int[] colours = {android.R.color.holo_red_dark, android.R.color.holo_blue_dark,
+                         android.R.color.holo_orange_dark};
 
-        ((ImageView) getView().findViewById(R.id.imageHolder)).setImageResource(quotes[rand]);
+        ((ImageView) getView().findViewById(R.id.imageHolder)).setImageResource(quotes[((new Random()).nextInt(quotes.length))]);
 
+        new CountDownTimer(1500, 1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish(){
+                if(i == colours.length){
+                    i = 0;
+                }
+                (getView().findViewById(R.id.btn_proceed)).setBackgroundColor(colours[i]);
+                i++;
+                start();
+            }
+        }.start();
     }
 }
